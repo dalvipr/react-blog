91c02eadf9533dee0d1f6bb804972b6d5e5b8e11
Pratik Dalvi <dalvi.pratik7@gmail.com>
2018-08-26 17:45:42 -0700

    Work In Progress

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Welcome from './components/Welcome';
+import Footer from './components/Footer';
 import registerServiceWorker from './registerServiceWorker';
+import CreateArticle from './components/CreateArticle';
+import Login from './components/Login/index';
+import SingleArticle from './components/SingleArticle/index';
+import Signup from './components/Signup/index';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+  <BrowserRouter>
+  <div>
+    <Navbar/>
+    <Route exact path='/' component={Welcome}/>
+    <Route path='/article/create' component={CreateArticle}/>
+		<Route path='/login' component={Login}/>
+		 <Route path='/signup' component={Signup}/>
+    <Route path='/article/:slug' component={SingleArticle}/>
+    <Footer/>
+  </div>
+</BrowserRouter>, document.getElementById('root'));
+
 registerServiceWorker();
